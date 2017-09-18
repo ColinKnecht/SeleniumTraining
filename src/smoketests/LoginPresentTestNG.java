202d2dce004c982087f2ff86bdc2ccfd51ae3328
ColinKnecht <cknecht87@gmail.com>
2017-09-18 12:14:39 -0400

    Adding Login Present Test

--- src/smoketests/LoginPresentTestNG.java
+++ src/smoketests/LoginPresentTestNG.java
@@ -1,6 +1,19 @@
 package smoketests;
 
-public class LoginPresentTestNG
-{
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
+public class LoginPresentTestNG {
+
+	@Test
+	public void loginElementsPresentTest() {
+		WebDriver driver = utilities.DriverFactory.open("chrome");
+		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+		
+		boolean loginEmailBox = driver.findElement(By.id("MainContent_txtUserName")).isDisplayed();
+		boolean passwordBox = driver.findElement(By.id("MainContent_txtPassword")).isDisplayed();
+		
+		
+	}
 }
