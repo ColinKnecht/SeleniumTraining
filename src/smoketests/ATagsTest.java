c707e0c3d780398a1bdb92a2e8e345a84512fbdd
ColinKnecht <cknecht87@gmail.com>
2017-09-18 14:29:53 -0400

    adding TestNG and xml files

--- src/smoketests/ATagsTest.java
+++ src/smoketests/ATagsTest.java
@@ -1,25 +1,37 @@
 package smoketests;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginPresentTestNG {
+public class ATagsTest {
 	WebDriver driver;
 	@Test
 	public void loginElementsPresentTest() {
 		System.out.println("Running Test"); 
+		boolean createAccountPresent = false;
 		String webUrl = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
 		driver.get(webUrl);
 		
-		boolean loginEmailBox = driver.findElement(By.id("MainContent_txtUserName")).isDisplayed();
-		boolean passwordBox = driver.findElement(By.id("MainContent_txtPassword")).isDisplayed();
+		List <WebElement> aElements = driver.findElements(By.tagName("a"));
+		int numberOfAElements = aElements.size();
+		System.out.println("Number of a tags = " + numberOfAElements);
 		
-		Assert.assertTrue(loginEmailBox, "Email Textbox present");
-		Assert.assertTrue(passwordBox, "Password textbox present");
+		for (WebElement aElement : aElements){
+			System.out.println(aElement.getText());
+			if (aElement.getText().equals("CREATE ACCOUNT")); {
+				createAccountPresent = true;
+			}
+		}
+		
+		Assert.assertTrue(createAccountPresent);
+
 	}
 	
 	@BeforeMethod
@@ -33,4 +45,8 @@ public class LoginPresentTestNG {
 		System.out.println("Closing Test");
 		driver.close();
 	}
+  @Test
+  public void aTagsLinksTest() {
+	  
+  }
 }
