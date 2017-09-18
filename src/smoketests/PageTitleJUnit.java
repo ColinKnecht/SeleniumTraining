a138ebe895a6d37a410e5480ba3832ba79aeea66
ColinKnecht <cknecht87@gmail.com>
2017-09-18 11:29:44 -0400

    Adding JUnit Assert methods

--- src/smoketests/PageTitleJUnit.java
+++ src/smoketests/PageTitleJUnit.java
@@ -1,6 +1,21 @@
 package smoketests;
 
-public class PageTitleJUnit
-{
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
+public class PageTitleJUnit {
+
+	@Test
+	public void pageTitleTest() {
+		String webUrl = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+		
+		WebDriver driver = utilities.DriverFactory.open("chrome");
+		driver.get(webUrl);
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "SDET Training | Account Management";
+		
+		Assert.assertEquals(expectedTitle, actualTitle);
+//		Assert.fail("we intentionally failed the test");
+	}
 }
