2b6b630371e7167fbd05983f308a5f2a5cfd8f79
ColinKnecht <cknecht87@gmail.com>
2017-09-18 12:14:16 -0400

    Adding TestNG Library

--- src/smoketests/PageTitleJUnit.java
+++ src/smoketests/PageTitleJUnit.java
@@ -1,21 +1,39 @@
 package smoketests;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 public class PageTitleJUnit {
+	//1. Declare variables and objects at CLASS level in order to access in multiple methods through program
+	WebDriver driver;
+	String webUrl = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
 
 	@Test
 	public void pageTitleTest() {
-		String webUrl = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+		System.out.println("Running the test");
 		
-		WebDriver driver = utilities.DriverFactory.open("chrome");
 		driver.get(webUrl);
 		String actualTitle = driver.getTitle();
 		String expectedTitle = "SDET Training | Account Management";
 		
 		Assert.assertEquals(expectedTitle, actualTitle);
 //		Assert.fail("we intentionally failed the test");
+	}
+	
+	@Before
+	public void setUp() {
+		System.out.println("Initializing the driver");
+		driver = utilities.DriverFactory.open("chrome");
+		
+	}
+	
+	@After
+	public void tearDown(){
+		System.out.println("Closing the test");
+		System.out.println("Closing the driver");
+		driver.close();
 	}
 }
