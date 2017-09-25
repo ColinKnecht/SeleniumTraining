17c822727ad8743116daef91054a3f2f458a1e3e
ColinKnecht <cknecht87@gmail.com>
2017-09-25 14:30:30 -0400

    Before Video 46

--- src/tests/NewAccountDDT.java
+++ src/tests/NewAccountDDT.java
@@ -2,6 +2,8 @@ package tests;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -16,38 +18,21 @@ import org.testng.Assert;
 
 @RunWith(value = Parameterized.class)
 public class NewAccountDDT {
-	String name;
-	String email;
-	String phone;
-	String gender;
-	String password;
-	String country;
-	boolean weeklyEmail;
-	boolean monthlyEmail;
-	boolean occasionalEmail;
+	String name,email, phone, gender, password, country;
+	boolean weeklyEmail, monthlyEmail, occasionalEmail;
+	WebElement nameElement, emailElement, phoneElement, passwordElement, confirmPasswordElement, countryElement, maleRadio,
+	femaleRadio, weeklyCheckbox, monthlyCheckbox, occasionalCheckbox, submitButton;
+	
+
+	WebDriver driver;
 
 	//This is out test method
 	@Test
 	public void newAccountTest() {
 		System.out.println("NEW RECORD " + name + " " + email + " " + phone + " " + gender + " "+ password + " " + country + " " + weeklyEmail 
 				+ " " +monthlyEmail + " " + occasionalEmail);
-
-		WebDriver driver = utilities.DriverFactory.open("chrome");
-		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
-		driver.findElement(By.linkText("Create Account")).click();
-		
-		WebElement nameElement = driver.findElement(By.name("ctl00$MainContent$txtFirstName"));
-		WebElement emailElement = driver.findElement(By.id("MainContent_txtEmail"));
-		WebElement phoneElement = driver.findElement(By.id("MainContent_txtHomePhone"));
-		WebElement passwordElement = driver.findElement(By.cssSelector("input[id='MainContent_txtPassword']"));
-		WebElement confirmPasswordElement = driver.findElement(By.name("ctl00$MainContent$txtVerifyPassword"));
-		WebElement countryElement = driver.findElement(By.id("MainContent_menuCountry"));
-		WebElement maleRadio = driver.findElement(By.name("ctl00$MainContent$Gender"));
-		WebElement femaleRadio = driver.findElement(By.id("MainContent_Female"));
-		WebElement weeklyCheckbox = driver.findElement(By.name("ctl00$MainContent$checkWeeklyEmail"));
-		WebElement monthlyCheckbox = driver.findElement(By.name("ctl00$MainContent$checkMonthlyEmail"));
-		WebElement occasionalCheckbox = driver.findElement(By.name("ctl00$MainContent$checkUpdates"));
-		WebElement submitButton = driver.findElement(By.name("MainContent_btnSubmit"));
+				
+		defineWebElements();
 		
 		nameElement.sendKeys(name);
 		emailElement.sendKeys(email);
@@ -97,6 +82,32 @@ public class NewAccountDDT {
 		submitButton.click();
 	}
 	
+	@Before
+	public void setUp(){
+		driver = utilities.DriverFactory.open("chrome");
+		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+		driver.findElement(By.xpath("//*[@id='ctl01']/div[3]/div[2]/div/div[2]/a")).click();
+	}
+	
+	@After
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	public void defineWebElements(){
+		nameElement = driver.findElement(By.name("ctl00$MainContent$txtFirstName"));
+		emailElement = driver.findElement(By.id("MainContent_txtEmail"));
+		phoneElement = driver.findElement(By.id("MainContent_txtHomePhone"));
+		passwordElement = driver.findElement(By.cssSelector("input[id='MainContent_txtPassword']"));
+		confirmPasswordElement = driver.findElement(By.name("ctl00$MainContent$txtVerifyPassword"));
+		countryElement = driver.findElement(By.id("MainContent_menuCountry"));
+		maleRadio = driver.findElement(By.name("ctl00$MainContent$Gender"));
+		femaleRadio = driver.findElement(By.id("MainContent_Female"));
+		weeklyCheckbox = driver.findElement(By.name("ctl00$MainContent$checkWeeklyEmail"));
+		monthlyCheckbox = driver.findElement(By.name("ctl00$MainContent$checkMonthlyEmail"));
+		occasionalCheckbox = driver.findElement(By.name("ctl00$MainContent$checkUpdates"));
+		submitButton = driver.findElement(By.name("ctl00$MainContent$btnSubmit"));
+	}
 	//This annotated method is designed to pass parameters into the class via constructor
 	@Parameters
 	public static List<String[]> getData() {
