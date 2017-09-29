241822e16f01b7a86a4da6e077664fa9598c8c7a
ColinKnecht <cknecht87@gmail.com>
2017-09-29 12:24:58 -0400

    Adding to booking tests -- before video 52

--- src/tests/Expedia.java
+++ src/tests/Expedia.java
@@ -1,7 +1,9 @@
 package tests;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,10 +11,21 @@ import org.testng.annotations.Test;
 public class Expedia {
 	WebDriver driver;
 	String browserType = "chrome";
+	String website = "https://www.expedia.com/";
+	String city = "New York, NY";
+	String checkIn = "10/02/2017";
+	String checkOut = "10/09/2017";
+	String numOfGuests = "2";
 	@Test
 	public void hotelReservation(){
 		//1. Search
-		
+		driver.findElement(By.id("tab-hotel-tab-hp")).click();
+		driver.findElement(By.id("hotel-destination-hp-hotel")).sendKeys(city);
+		driver.findElement(By.id("hotel-checkin-hp-hotel")).sendKeys(checkIn);
+		driver.findElement(By.id("hotel-checkout-hp-hotel")).clear();
+		driver.findElement(By.id("hotel-checkout-hp-hotel")).sendKeys(checkOut);
+		new Select(driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[4]/div[4]/label/select"))).selectByValue(numOfGuests);
+		driver.findElement(By.xpath("//*[@id='gcw-hotel-form-hp-hotel']/div[9]/label/button")).click();
 		//2.Modify Search results page
 		
 		//3. analyze the results and make our selection
@@ -27,7 +40,7 @@ public class Expedia {
 	@BeforeMethod
 	public void setup(){
 		driver = utilities.DriverFactory.open(browserType);
-		driver.get("https://www.expedia.com/");
+		driver.get(website);
 	}
 	
 	@AfterMethod
