c329b61588d2f1b9a5c77e2cdafe188eb70e1836
ColinKnecht <cknecht87@gmail.com>
2017-10-02 11:58:27 -0400

    Implementing Page Object Model -- Before Video 59

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -3,20 +3,23 @@ package tests;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
+import pages.LoginPage;
+
 public class Login { //Using page object model
 	
 	@Test
 	public void loginTestPOM(){
 		WebDriver driver = utilities.DriverFactory.open("chrome");
 		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+		driver.manage().window().maximize();
 		//initialize Driver
-		
+		LoginPage loginPage = new LoginPage(driver);
 		//enter login info
-		
+		loginPage.setUserName("Johnny");
 		//get confirmation
 		
 		//close the driver
-		driver.quit();
+//		driver.quit();
 	}
 
 
