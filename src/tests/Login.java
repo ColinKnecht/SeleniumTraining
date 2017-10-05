354d004e7ccb2d6d5902a520a5f8f44e19eaf35d
ColinKnecht <cknecht87@gmail.com>
2017-10-05 12:56:13 -0400

    Before Video 61

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -1,8 +1,10 @@
 package tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
+import pages.DashboardPage;
 import pages.LoginPage;
 
 public class Login { //Using page object model
@@ -15,8 +17,15 @@ public class Login { //Using page object model
 		//initialize Driver
 		LoginPage loginPage = new LoginPage(driver);
 		//enter login info
-		loginPage.setUserName("Johnny");
+		loginPage.setUserName("tim@testemail.com");
+		loginPage.setPassword("trpass");
+		loginPage.clickSubmit();
+		
 		//get confirmation
+		DashboardPage dashboardPage = new DashboardPage(driver);
+		String confirm = dashboardPage.printConfirmation();
+		System.out.println(confirm);
+		Assert.assertTrue(confirm.contains("Success"));
 		
 		//close the driver
 //		driver.quit();
