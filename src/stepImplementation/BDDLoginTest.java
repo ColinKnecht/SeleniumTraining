457c337d0ee98c8b09f2fcde92a3765e88fb75c6
ColinKnecht <cknecht87@gmail.com>
2017-10-06 12:05:50 -0400

    Before Video 68

--- src/stepImplementation/BDDLoginTest.java
+++ src/stepImplementation/BDDLoginTest.java
@@ -1,5 +1,7 @@
 package stepImplementation;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import cucumber.api.java.en.Given;
@@ -13,15 +15,27 @@ public class BDDLoginTest {
 	@Given("^user is on the login page$")
 	public void user_is_on_the_login_page(){
 		System.out.println("User is on the login page");
+		//open driver
+		driver = utilities.DriverFactory.open("chrome");
+		driver.manage().window().maximize();
+		driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+		
+		//navigate to login page
 	}
 	
 	@When("^user enters correct username and correct password$")
 	public void user_enters_correct_username_and_correct_password(){
 		System.out.println("User enters correct username and password");
+		driver.findElement(By.name("ctl00$MainContent$txtUserName")).sendKeys("tim@testemail.com");
+		driver.findElement(By.name("ctl00$MainContent$txtPassword")).sendKeys("trpass");
+		driver.findElement(By.name("MainContent_btnLogin")).click();
+		
 	}
 	
 	@Then("^user gets confiramtion$")
 	public void user_gets_confirmation(){
 		System.out.println("User gets confirmation");
+		driver.findElement(By.id("conf_message")).getText();
+		Assert.assertTrue(driver.findElement(By.id("conf_message")).getText().contains("success"));
 	}
 }
